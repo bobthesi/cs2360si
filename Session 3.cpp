bb2775834d951f17fff463c17ee892d1beef20b8
BhavdeepSinghB <bhavdeepsmail@gmail.com>
2018-04-17 14:19:53 -0700

    Added second file, some changes

---
+++ Session 3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	/*int x = 5;
+	cout<<x<<endl;
+	int* p = &x;
+	cout<<p<<endl;
+	cout<<*p<<endl;
+	*/
+
+	//Declaring an array
+	int arr[5] = {1,2,3,5,7};
+	
+
+	cin.get();
+
+
+}
